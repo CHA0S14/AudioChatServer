1eb03aeff3a5ba025108a91356748fa8ff85ccf2
Chaos <Chaos@CHAOS>
2014-06-05 17:54:30 +0200

    solo queda la interfaz de servidor pero ya sta acabado ^^

--- AudioChatServidor/src/es/cios/audiochat/servicios/AudioChatService.java
+++ AudioChatServidor/src/es/cios/audiochat/servicios/AudioChatService.java
@@ -16,9 +16,11 @@ import es.cios.audiochat.hilos.Conexion;
 import es.cios.audiochat.hilos.HiloCliente;
 
 /**
- * Clase que se encarga de ser el pto intermedio entre la conexion y la logica del servidor
+ * Clase que se encarga de ser el pto intermedio entre la conexion y la logica
+ * del servidor
+ * 
  * @author Ismael Ortega
- *
+ * 
  */
 public class AudioChatService {
 	private static List<Canal> canales = new ArrayList<Canal>();
@@ -35,7 +37,9 @@ public class AudioChatService {
 
 	/**
 	 * Añade un cliente al main cuando se conecta
-	 * @param socket el socket del cliente
+	 * 
+	 * @param socket
+	 *            el socket del cliente
 	 */
 	public static void addCliente(Socket socket) {
 		Cliente cliente = new Cliente();
@@ -49,13 +53,17 @@ public class AudioChatService {
 	}
 
 	/**
-	 * escribe el mensaje para el cliente dependiendo que que quiera hacer sera una clase distinta
-	 * @param obj objeto a enviar
-	 * @param canalNum canal al cual se va a enviar el mensaje
-	 * @param subCanalNum si es distinto de -1 entonces se envia aqui en vez de al canal
+	 * escribe el mensaje para el cliente dependiendo que que quiera hacer sera
+	 * una clase distinta
+	 * 
+	 * @param obj
+	 *            objeto a enviar
+	 * @param canalNum
+	 *            canal al cual se va a enviar el mensaje
+	 * @param subCanalNum
+	 *            si es distinto de -1 entonces se envia aqui en vez de al canal
 	 */
-	public static void escribirMensaje(Object obj, int canalNum,
-			int subCanalNum) {
+	public static void escribirMensaje(Object obj, int canalNum, int subCanalNum) {
 		Canal canal = canales.get(canalNum);
 		List<Cliente> clientes;
 
@@ -74,10 +82,15 @@ public class AudioChatService {
 
 	/**
 	 * le coloca un nombre al cliente
-	 * @param nombre nombre a poner
-	 * @param socketAddress ip del cliente utilizado como id de diferenciacion
-	 * @param canalNum canal en el que se encuentra
-	 * @param subCanalNum sub canal en el que se encuentra si no esta en ninguno es -1
+	 * 
+	 * @param nombre
+	 *            nombre a poner
+	 * @param socketAddress
+	 *            ip del cliente utilizado como id de diferenciacion
+	 * @param canalNum
+	 *            canal en el que se encuentra
+	 * @param subCanalNum
+	 *            sub canal en el que se encuentra si no esta en ninguno es -1
 	 */
 	private static void addNombre(Nombre nombre, SocketAddress socketAddress,
 			int canalNum, int subCanalNum) {
@@ -93,22 +106,28 @@ public class AudioChatService {
 				Cliente cliente = clientes.get(cnum);
 				if (cliente.getSocketAddress().equals(socketAddress)) {
 					cliente.setNombre(nombre.getNombre());
-					buscar=false;
+					buscar = false;
 				}
 				cnum++;
 			} catch (IndexOutOfBoundsException e) {
-				buscar=false;
+				buscar = false;
 			}
 		}
 		actualizar();
 	}
 
 	/**
-	 * se encarga de decidir que haccion hacer dependiendo del objeto que ha enviado el cliente
-	 * @param object objeto que ha enviado
-	 * @param canal canal en el que esta
-	 * @param subCanal sub canal en el que esta si no esta en ninguno es -1
-	 * @param socketAddress ip del cliente utilizado como id de diferenciacion
+	 * se encarga de decidir que haccion hacer dependiendo del objeto que ha
+	 * enviado el cliente
+	 * 
+	 * @param object
+	 *            objeto que ha enviado
+	 * @param canal
+	 *            canal en el que esta
+	 * @param subCanal
+	 *            sub canal en el que esta si no esta en ninguno es -1
+	 * @param socketAddress
+	 *            ip del cliente utilizado como id de diferenciacion
 	 */
 	public static void recibirObjeto(Object object, int canal, int subCanal,
 			SocketAddress socketAddress) {
@@ -116,26 +135,33 @@ public class AudioChatService {
 			escribirMensaje(object, canal, subCanal);
 		} else if (object instanceof Nombre) {
 			addNombre((Nombre) object, socketAddress, canal, subCanal);
-		}else if (object instanceof Finalizar){
-			borrarCliente(canal,subCanal,socketAddress);
-		}else if(object instanceof Cliente){	
-			moverCliente(canal,subCanal, socketAddress, (Cliente) object);
-		}else if (object instanceof CanalMod){
+		} else if (object instanceof Finalizar) {
+			borrarCliente(canal, subCanal, socketAddress);
+		} else if (object instanceof Cliente) {
+			moverCliente(canal, subCanal, socketAddress, (Cliente) object);
+		} else if (object instanceof CanalMod) {
 			CanalMod canalMod = (CanalMod) object;
-			if(canalMod.isNuevo()){
+			if (canalMod.isNuevo()) {
 				cambiarNombreCanal(canalMod);
-			}else{
-				crearCanal(canal,subCanal,canalMod,socketAddress);
+			} else {
+				crearCanal(canal, subCanal, canalMod, socketAddress);
 			}
 		}
 	}
-	
+
 	/**
-	 * mueve un cliente de un canal a otro y ademas si el canal esta vacio despues de irse el cliente este se elimina
-	 * @param canalNum canal en el que esta
-	 * @param subCanalNum subCanal en el que esta si no esta en ninguno es -1
-	 * @param socketAddress ip del cliente utilizado como id de diferenciacion
-	 * @param clienteMov objeto con el canal y el subcanal al que se quiere mover el cliente
+	 * mueve un cliente de un canal a otro y ademas si el canal esta vacio
+	 * despues de irse el cliente este se elimina
+	 * 
+	 * @param canalNum
+	 *            canal en el que esta
+	 * @param subCanalNum
+	 *            subCanal en el que esta si no esta en ninguno es -1
+	 * @param socketAddress
+	 *            ip del cliente utilizado como id de diferenciacion
+	 * @param clienteMov
+	 *            objeto con el canal y el subcanal al que se quiere mover el
+	 *            cliente
 	 */
 	private static void moverCliente(int canalNum, int subCanalNum,
 			SocketAddress socketAddress, Cliente clienteMov) {
@@ -149,27 +175,24 @@ public class AudioChatService {
 		while (buscar) {
 			try {
 				Cliente cliente = clientes.get(cnum);
-				if (cliente.getSocketAddress().equals(socketAddress)) {		
-					clientes.remove(cliente);	
-					Canal canal2 = canal;					
+				if (cliente.getSocketAddress().equals(socketAddress)) {
+					clientes.remove(cliente);
+					Canal canal2 = canal;
 					canal = canales.get(clienteMov.getCanal());
 					if (clienteMov.getSubCanal() != -1) {
 						canal = canal.getSubCanal(clienteMov.getSubCanal());
-					}							
-					
+					}
+
 					cliente.setCanal(clienteMov.getCanal());
 					cliente.setSubCanal(clienteMov.getSubCanal());
-					
-					boolean valeSub = clienteMov.getSubCanal()>subCanalNum;
-					boolean vale = clienteMov.getCanal()>canalNum;
-					
+
 					canal.getClientes().add(cliente);
-					eliminarCanalMover(cliente, canal2, canalNum, valeSub, vale);					
-					buscar=false;
+					eliminarCanalMover(canal2, canalNum, subCanalNum);
+					buscar = false;
 				}
 				cnum++;
 			} catch (IndexOutOfBoundsException e) {
-				buscar=false;
+				buscar = false;
 			}
 		}
 		actualizar();
@@ -177,47 +200,79 @@ public class AudioChatService {
 
 	/**
 	 * elimina un canal si no tiene clientes ni subcanales con clientes
-	 * @param cliente cliente que se ha movido
-	 * @param canal canal que se va a comprobar
-	 * @param subCanalNum numero del subCanal que se  va a comprobar
+	 * 
+	 * @param cliente
+	 *            cliente que se ha movido
+	 * @param canal
+	 *            canal que se va a comprobar
+	 * @param subCanalNum
+	 *            numero del subCanal que se va a comprobar
 	 */
-	private static void eliminarCanalMover(Cliente cliente, Canal canal, int canalNum, boolean valeSub, boolean vale) {
-		if(canal.getNumClient()==0 && canal.getNumSubCanal()==0){
-			if(canal instanceof SubCanal){
+	private static void eliminarCanalMover(Canal canal, int canalNum,
+			int subCanalNum) {
+		if (canal.getNumClient() == 0 && canal.getNumSubCanal() == 0) {
+			if (canal instanceof SubCanal) {
 				canales.get(canalNum).getSubCanales().remove(canal);
-				if(vale){
+				reasignarCanalNum(canalNum, subCanalNum);
+				eliminarCanalMover(canales.get(canalNum), canalNum, subCanalNum);
+			} else if (canal instanceof Canal) {
+				if (!canal.getName().equals("C: Main chanel")) {
+					canales.remove(canal);
+					reasignarCanalNum(canalNum, -1);
+				}
+			}
+		}
+	}
+
+	private static void reasignarCanalNum(int canalNum, int subCanalNum) {
+		List<Cliente> clientes;
+		if (subCanalNum > -1) {
+			Canal canal = canales.get(canalNum);
+			List<SubCanal> subCanales = canal.getSubCanales();
+			for (int i = subCanalNum; i < subCanales.size(); i++) {
+				clientes = subCanales.get(i).getClientes();
+				for (Cliente cliente : clientes) {
 					cliente.setSubCanal(cliente.getSubCanal()-1);
 				}
-				eliminarCanalMover(cliente, canales.get(canalNum), canalNum, valeSub, vale);
-			}else if (canal instanceof Canal){
-				if(!canal.getName().equals("C: Main chanel")){
-					canales.remove(canal);
-					if(vale){
-						cliente.setCanal(cliente.getCanal()-1);
-					}
+			}
+		} else {
+			for (int i = canalNum; i < canales.size(); i++) {
+				clientes = canales.get(i).getClientes();
+				for (Cliente cliente : clientes) {
+					cliente.setCanal(cliente.getCanal()-1);
 				}
 			}
 		}
 	}
 
 	/**
-	 * crea un canal o un subcanal dependiendo de la variable canal del objeto canalMod
-	 * @param canalMod objeto con el canal en el cual se quiere crear el subcanal si es un subcanal lo que quieres crear, ademas tiene el nombre del canal en cuestion
+	 * crea un canal o un subcanal dependiendo de la variable canal del objeto
+	 * canalMod
+	 * 
+	 * @param canalMod
+	 *            objeto con el canal en el cual se quiere crear el subcanal si
+	 *            es un subcanal lo que quieres crear, ademas tiene el nombre
+	 *            del canal en cuestion
+	 * @param canalNum
+	 * @param subCanalNum
+	 * @param socketAddress
 	 */
-	private static void crearCanal(int canalNum, int subCanalNum, CanalMod canalMod, SocketAddress socketAddress) {
+	private static void crearCanal(int canalNum, int subCanalNum,
+			CanalMod canalMod, SocketAddress socketAddress) {
 		Cliente clienteMov = new Cliente();
-		if (canalMod.getCanal()>-1){
-			List<SubCanal> canales = AudioChatService.canales.get(canalMod.getCanal()).getSubCanales();
+		if (canalMod.getCanal() > -1) {
+			List<SubCanal> canales = AudioChatService.canales.get(
+					canalMod.getCanal()).getSubCanales();
 			SubCanal subCanal = new SubCanal();
 			subCanal.setName(canalMod.getNombre());
 			canales.add(subCanal);
 			clienteMov.setCanal(canalMod.getCanal());
-			clienteMov.setSubCanal(canales.size()-1);
-		}else{
+			clienteMov.setSubCanal(canales.size() - 1);
+		} else {
 			Canal canal2 = new Canal();
 			canal2.setName(canalMod.getNombre());
 			AudioChatService.canales.add(canal2);
-			clienteMov.setCanal(canales.size()-1);
+			clienteMov.setCanal(canales.size() - 1);
 			clienteMov.setSubCanal(-1);
 		}
 		moverCliente(canalNum, subCanalNum, socketAddress, clienteMov);
@@ -226,11 +281,14 @@ public class AudioChatService {
 
 	/**
 	 * cambia el nombre de un canal
-	 * @param canalMod contiene elnumero del canal y el numero del subcanal si tubiese (si no es -1), ademas del nombre del canal
+	 * 
+	 * @param canalMod
+	 *            contiene elnumero del canal y el numero del subcanal si
+	 *            tubiese (si no es -1), ademas del nombre del canal
 	 */
 	private static void cambiarNombreCanal(CanalMod canalMod) {
 		Canal canal = canales.get(canalMod.getCanal());
-		if(canalMod.getSubCanal()>-1){
+		if (canalMod.getSubCanal() > -1) {
 			canal = canal.getSubCanal(canalMod.getSubCanal());
 		}
 		canal.setName(canalMod.getNombre());
@@ -239,9 +297,14 @@ public class AudioChatService {
 
 	/**
 	 * borra un cliente de un canal
-	 * @param canalNum numero del canal en el que se situa el cliente
-	 * @param subCanalNum numero del subCanal en el que se situa el cliente si no esta en uno es -1
-	 * @param socketAddress ip del cliente utilizado como id de diferenciacion
+	 * 
+	 * @param canalNum
+	 *            numero del canal en el que se situa el cliente
+	 * @param subCanalNum
+	 *            numero del subCanal en el que se situa el cliente si no esta
+	 *            en uno es -1
+	 * @param socketAddress
+	 *            ip del cliente utilizado como id de diferenciacion
 	 */
 	private static void borrarCliente(int canalNum, int subCanalNum,
 			SocketAddress socketAddress) {
@@ -258,21 +321,22 @@ public class AudioChatService {
 				cliente = clientes.get(cnum);
 				if (cliente.getSocketAddress().equals(socketAddress)) {
 					clientes.remove(cliente);
-					eliminarCanalMover(cliente, canal, subCanalNum, false, false);					
-					buscar=false;
+					eliminarCanalMover(canal, canalNum, subCanalNum);
+					buscar = false;
 				}
 				cnum++;
 			} catch (IndexOutOfBoundsException e) {
-				buscar=false;
+				buscar = false;
 			}
 		}
-		actualizar();		
+		actualizar();
 	}
 
 	/**
-	 * metodo que se encarga de actualizar la lista de clientes, canales y subcanales de todos los clientes
+	 * metodo que se encarga de actualizar la lista de clientes, canales y
+	 * subcanales de todos los clientes
 	 */
-	public static void actualizar(){
+	public static void actualizar() {
 		for (Canal canal : canales) {
 			List<Cliente> clientes = canal.getClientes();
 			for (Cliente cliente : clientes) {
